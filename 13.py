3b437081efc67957d798d06e4c0a9da0d00e6035
julian <juliii@gmail.com>
2020-12-18 03:30:10 +0100

    solution for day 13.2

--- 13.py
+++ 13.py
@@ -20,15 +20,48 @@ minWaitIdx = buses.index(minTimestamp)
 print(f'waiting time from {earliestDeparture}: bus {busLabels[minWaitIdx]} with {minTimestamp - earliestDeparture}mins')
 print(f'solution: bus label * wait time = {busLabels[minWaitIdx] * (minTimestamp - earliestDeparture)}')
 print()
+
 # part 2
+
+"""    ,            _..._            ,
+      {'.         .'     '.         .'}
+     { ~ '.      _|=    __|_      .'  ~}
+    { ~  ~ '-._ (___________) _.-'~  ~  }
+   {~  ~  ~   ~.'           '. ~    ~    }
+  {  ~   ~  ~ /   /\     /\   \   ~    ~  }
+  {   ~   ~  /    __     __    \ ~   ~    }
+   {   ~  /\/  -<( o)   ( o)>-  \/\ ~   ~}
+    { ~   ;(      \/ .-. \/      );   ~ }
+     { ~ ~\_  ()  ^ (   ) ^  ()  _/ ~  }
+      '-._~ \   (`-._'-'_.-')   / ~_.-'
+          '--\   `'._'"'_.'`   /--'
+              \     \`-'/     /
+               `\    '-'    /'
+                 `\       /'
+                   '-...-'
+
+that's me right now. i'm a clown because i chose to solve this part of the problem like this.
+look at my red nose. isn't it stupid?"""
+
 
 offsets = []
 offset = 0
 for c in input.splitlines()[1].split(','):
-    if c == 'x':
-        offset += 1
-    else:
+    if c != 'x':
         offsets.append(offset)
+    offset += 1
 
 print('labels:', busLabels)
 print('offsets:', offsets)
+
+print('{ ', end='')
+for i in range(0, len(busLabels)):
+    print(f'Mod[t+{offsets[i]}, {busLabels[i]}] == 0', end='')
+    if i != len(busLabels) - 1:
+        print(', ', end='')
+print('}')
+
+# wolframalpha says that this solves to
+#   840493039281088 + 896915841102983 n, n in Z
+# so for a meaningful solution of the first possible time, we set n = 0
+# our solution is hence 840493039281088.
